e95cdb042eda0825807e442881f807bdd8a45e2b
affe <affeisme@gmail.com>
2019-03-27 19:06:56 -0700

    0327 added moving scripts and managers

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public int m_NumRoundsToWin = 3;
+    // public CameraControl m_CameraControl; 
+    public Text m_MessageText;
+    public GameObject m_Chick;
+    public GameObject m_Ruby;
+    public PlayerManager [] m_Players;
+
+
+    private int m_CurrentRounderNumber;
+    private PlayerManager m_RoundWinner;
+    private PlayerManager m_GameWinner;
+
+	// Use this for initialization
+	private void Start () {
+
+
+        SpawnChicks();
+        SetCameraTargets();
+
+        StartCoroutine(GameLoop());
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+
+
+    private void SpawnChicks()
+    {
+        // create the players on the map
+    }
+
+    private void SetCameraTargets()
+    {
+        // use to assign camera control the game objects' transform matrix, not needed if we don't move camera
+    }
+
+    private IEnumerator GameLoop ()
+    {
+        yield return StartCoroutine(RoundStarting());
+
+        yield return StartCoroutine(RoundPlaying());
+
+        yield return StartCoroutine(RoundEnding());
+
+        if (m_GameWinner != null)
+        {
+            //TODO : this is used 
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            StartCoroutine(GameLoop());
+        }
+    }
+
+    private IEnumerator RoundStarting ()
+    {
+        ResetAllPlayers();
+        DisablePlayerControl();
+
+        // if we use camera, delete the following comment
+        // m_CameraControl.SetStartPositionAndSize();
+
+        m_CurrentRounderNumber++;
+        m_MessageText.text = "ROUND" + m_CurrentRounderNumber;
+
+        yield return new WaitForSeconds(1); // the actual number here can be adjusted
+    }
+
+    private IEnumerator RoundPlaying()
+    {
+        EnablePlayerControl();
+
+        // if we use camera, delete the following comment
+        // m_CameraControl.SetStartPositionAndSize();
+
+        
+        m_MessageText.text = string.Empty;
+
+        yield return new WaitForSeconds(1); // the actual number here can be adjusted
+    }
+
+    private IEnumerator RoundEnding()
+    {
+        DisablePlayerControl();
+
+        m_RoundWinner = null;
+        m_RoundWinner = GetRoundWinner();
+
+        if (m_RoundWinner != null)
+            m_RoundWinner.m_Wins++;
+
+        m_GameWinner = GetGameWinner();
+
+        string message = GetEndMessage();
+        m_MessageText.text = message;
+
+        m_MessageText.text = string.Empty;
+
+        yield return new WaitForSeconds(1); // the actual number here can be adjusted
+    }
+
+
+    // for disable/enable player control
+    private void EnablePlayerControl()
+    {
+
+    }
+
+    private void DisablePlayerControl()
+    {
+
+    }
+
+    // reset all the players : chick and ruby
+    private void ResetAllPlayers()
+    {
+
+    }
+
+
+    // some util function
+    private PlayerManager GetGameWinner()
+    {
+        // find out if there is a winner, if not return null
+    }
+
+    private PlayerManager GetRoundWinner()
+    {
+        // find out if there is a winner, if not return null
+    }
+
+    private string GetEndMessage()
+    {
+        // return what need to be played when a round ends
+        return null;
+    }
+}
---
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerManager {
+
+    // if we want use spawn point, cancel the comment
+    // public Transform m_SpawnPoint;
+
+    // member variable we want to expose to other scripts
+    [HideInInspector] public int m_PlayerNumber;
+    [HideInInspector] public string m_PlayerText; // played on top of each object's head
+    [HideInInspector] public GameObject m_Instance; // can be either a chick or a ruby
+    [HideInInspector] public int m_Wins; // how many round has this player winned
+
+    private ChickMove m_Movement;
+    private GameObject m_MaskCanvas; // used to block the checker board whem menu is on
+
+	// Use this for initialization
+	public void Setup () {
+        // set the m_Instance in other scripts
+        m_Movement = m_Instance.GetComponent<ChickMove>();
+        m_Movement.m_PlayerNumber = m_PlayerNumber; // also needs to set in other scripts
+
+        m_PlayerText = ""; // TODO : needs to assign a default player text
+	}
+
+    public void DisableControl()
+    {
+        m_Movement.enabled = false;
+
+        // TODO : what is the MaskCanvas here means ?
+    }
+
+    public void EnableControl()
+    {
+        m_Movement.enabled = true;
+    }
+
+    public void Reset()
+    {
+        // TODO : what is needed to be done in order to reset?
+    }
+     
+}
